226c0fa445b0652e26db3aa78d44cf7eba6d73f0
Vinicius Molina <vinicius.molina@einstein.br>
2023-01-24 09:16:44 -0300

    feat: add order entity

---
+++ src/entity/order/order.ts
@@ -0,0 +1,33 @@
+import Address from "../address";
+import OrderItem from "./order_item";
+
+export default class Order {
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[];
+    private _total: number;
+
+    constructor(id: string, customerId: string, items: OrderItem[]) {
+        this._id = id;
+        this._customerId = customerId;
+        this._items = items;
+        this._total = this.total();
+        this.validate();
+    }
+
+    total(): number {
+        return this._items.reduce((acc, item) => acc + item.total(), 0);
+    }
+
+    validate(): void {
+        if(this._id.length === 0) {
+            throw new Error('Id is required');
+        }
+        if(this._customerId.length === 0) {
+            throw new Error('CustomerId is required');
+        }
+        if(this._items.length === 0) {
+            throw new Error('Item quantity must be greater than zero');
+        }
+    }
+}
