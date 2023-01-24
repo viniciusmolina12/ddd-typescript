4056fea953335bf9e3778e375b72c3cb24b8f0fb
Vinicius Molina <vinicius.molina@einstein.br>
2023-01-24 09:16:31 -0300

    test: add order tests

---
+++ src/entity/order/order.spec.ts
@@ -0,0 +1,32 @@
+import Order from "./order"
+import OrderItem from "./order_item"
+
+describe('Order unit tests', () => {
+    it('should throw error when id is empty', () => {
+        expect(() => {
+            const order = new Order('', '123', [])
+        }).toThrowError('Id is required')
+    })
+    it('should throw error when customerId is empty', () => {
+        expect(() => {
+            const order = new Order('123', '', [])
+        }).toThrowError('Id is required')
+    })
+    it('should throw error when item is empty', () => {
+        expect(() => {
+            const order = new Order('123', '123', [])
+        }).toThrowError('Item quantity must be greater than zero')
+    })
+
+    it('should calculate total', () => {
+        const item = new OrderItem('123', 'Item 1', 200, 1);
+        const item2 = new OrderItem('321', 'Item 2', 150, 1);
+        const order = new Order('1', '1', [item]);
+        let total = order.total();
+        expect(total).toBe(200);
+        const order2 = new Order('2', '2', [item, item2]);
+        total = order2.total();
+        expect(total).toBe(350);
+    })
+
+})
