1f5e4407ee38d0d4def8c472f38d289cb3def649
Vinicius Molina <vinicius.molina@einstein.br>
2023-01-27 21:50:00 -0300

    test: ensure order calculates total correctly

--- src/entity/order/order.spec.ts
+++ src/entity/order/order.spec.ts
@@ -19,14 +19,21 @@ describe('Order unit tests', () => {
     })
 
     it('should calculate total', () => {
-        const item = new OrderItem('123', 'Item 1', 200, 1);
-        const item2 = new OrderItem('321', 'Item 2', 150, 1);
+        const item = new OrderItem('123', 'Item 1', 200, 2, "p1");
+        const item2 = new OrderItem('321', 'Item 2', 150, 2, "p2");
         const order = new Order('1', '1', [item]);
         let total = order.total();
-        expect(total).toBe(200);
+        expect(total).toBe(400);
         const order2 = new Order('2', '2', [item, item2]);
         total = order2.total();
-        expect(total).toBe(350);
+        expect(total).toBe(700);
+    })
+
+    it('should throw a error if the item quantity is less or equal 0', () => {
+        expect(() => {
+            const item = new OrderItem('123', 'Item 1', 200, 0, "p1");
+            const order = new Order('1', '1', [item]);
+        }).toThrowError('Quantity must be greater than 0')
     })
 
 })
