2e0b0b4307bfe3fa58476f38e379dddba84601cc
Vinicius Molina <vinicius.molina@einstein.br>
2023-04-13 21:22:02 -0300

    feat: add product factory

---
+++ src/domain/product/factory/product.factory.spec.ts
@@ -0,0 +1,12 @@
+import ProductFactory from "./product.factory";
+
+describe('Product factory unit test', () => {
+    it('should create a product type a', () => {
+        const product = ProductFactory.create("Product A", 1);
+        expect(product.id).toBeDefined();
+        expect(product.name).toBe('Product A');
+        expect(product.price).toBe(1);
+        expect(product.constructor.name).toBe("Product");
+    })
+
+})
